69f6d3a3817420963f4170385a12cb0793fe5169
Menace791 <manas.s.sara@gmail.com>
2023-11-30 12:11:14 -0800

    god

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,78 @@
+#include <iostream>
+#include <fstream>
 #include <SFML/Graphics.hpp>
+
+std::string pathToAssets;
+
+const float windowLength = 800.0f;
+const float windowHeight = 600.0f;
+const float ballDiameter = 5.0f;
+
+// OBJECTIFY THIS
+const float ballVelocity = 10.0f;
+
+enum Side {Left, Right};
+Side winningSide = Right;
+
+sf::RenderWindow window;
+// sf::CircleShape ball(ballDiameter);
+
+// void InitWindow()
+// {
+//     std::cout << "Initializing window\n";
+//     window.create(sf::VideoMode(windowLength, windowHeight), "PONG!", sf::Style::Titlebar | sf::Style::Close | sf::Style::Resize);
+//     window.setFramerateLimit(60);
+//     std::cout << "Window initialized\n";
+// }
+
+// void BallReset()
+// {
+//     ball.setPosition(sf::Vector2f(windowLength/2-ballDiameter, windowHeight/2-ballDiameter));
+// }
+
+// void Serve()
+// {
+
+// }
+
+
+// void SpaceDebugCommand()
+// {
+//     BallReset();
+// }
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+    // std::cout << "Beginning of main\n";
 
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-        }
+    // InitWindow();
 
-        window.clear();
-        window.display();
-    }
+    //  #if _WIN32
+    //     pathToAssets = "../../../src/assets/";
+    // #elif __APPLE__
+    //     pathToAssets = "../../src/assets/";
+    // #endif
+
+    // while (window.isOpen())
+    // {
+    //     sf::Event event;
+    //     while (window.pollEvent(event))
+    //     {
+    //         if (event.type == sf::Event::Closed)
+    //         {
+    //             window.close();
+    //         }
+    //     }
+
+    //     window.clear();
+
+    //     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+    //     {
+    //         SpaceDebugCommand();
+    //     }
+
+    //     window.draw(ball);
+
+    //     window.display();
+    // }
 }
